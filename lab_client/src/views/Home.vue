0b64dbd401ed013436814cc84cb9fd99552ce04d
Caio Santos <caiobs10@gmail.com>
2023-01-25 13:18:29 -0300

    Update Home.vue

--- lab_client/src/views/Home.vue
+++ lab_client/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-    <router-link to="/aq/decomol">AQ</router-link>
-    <router-link to="/fab/decomol">Fab</router-link>
+    <div class="home-container">
+        <nav>
+            <router-link to="/aq/decomol">AQ</router-link> |
+            <router-link to="/fab/decomol">Fab</router-link>
+        </nav>
+        <div class="content">
+            
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,3 +15,51 @@
         name: 'HomeView',
     }
 </script>
+
+<style>
+    nav {
+        display: flex;
+        padding: 15px;
+        height: 10vh;
+        border-bottom: 3px white solid;
+        justify-content: center;
+        align-items: center;
+        background-color: #282A35;
+        gap: 5px;
+        color: white;
+    }
+
+    nav a {
+        font-weight: bold;
+        color: white;
+        border: 1px solid transparent;
+    }
+
+    nav a:hover{
+        color: white;
+        border-bottom: 1px solid darkgray;
+        transition-delay: 50ms;
+    }
+
+    nav a.router-link-active {
+        color: lightgreen;
+}
+
+    .home-container { 
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        background-color: lightgray;
+        color: black;
+    }
+
+    .content{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        color: black;   
+        min-height: 90vh;
+    } 
+</style>
