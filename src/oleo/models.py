c296985decb65f3c2e1bbce362f07a5ae8a92322
Caio Santos <caiobs10@gmail.com>
2023-02-15 15:07:57 -0300

    update oleodateTime

--- src/oleo/models.py
+++ src/oleo/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Oleo(models.Model):
     consumo = models.IntegerField()
-    data_analise = models.DateTimeField(auto_now=True)
+    data_analise = models.DateField(auto_now=False, auto_now_add=False)
--- src/oleo/serializers.py
+++ src/oleo/serializers.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest_framework import serializers, fields
 from .models import Oleo
 
 class OleoSerializer(serializers.ModelSerializer):
+    data_analise = serializers.DateField(format="%d-%m-%Y", input_formats=['%d-%m-%Y', 'iso-8601'])
     class Meta:
         model = Oleo
         fields = ['id', 'consumo', 'data_analise']
